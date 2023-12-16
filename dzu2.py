4b62a8258343156ad76a450cba155cb2ce8c5133
matdfef <136637632+matdfef@users.noreply.github.com>
2023-12-16 11:51:27 +0200

    Add files via upload

---
+++ dzu2.py
@@ -0,0 +1,14 @@
+class Borus:
+    height = 175
+    age = 34
+class Dmytro(Borus):
+    height = 178
+    age = 25
+class Matvij(Borus):
+    height = 178
+    age = 13
+class Marco(Matvij,Dmytro):
+    height = 78
+    age = 6
+    
+ 
